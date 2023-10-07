695a0510edaa3998be5c6a3020aa1661261f7e52
DrakeAHoffman <drakehoffman99@gmail.com>
2023-10-06 18:28:28 -0700

    finished project

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+
+test('renders text MickeyMouse', () => {
+  render(<App />)
+  const nameElement = screen.getByText(/mickey mouse/i)
+  expect(nameElement).toBeInTheDocument()
+});
